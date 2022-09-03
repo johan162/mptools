7e7586473ee6c348d40836e22328952494c92294
Johan Persson <johan162@gmail.com>
2022-09-03 09:23:02 +0200

    Cleanup, added duplicated name check

--- mpn.sh
+++ mpn.sh
@@ -5,13 +5,17 @@
 # All tools released under MIT License. See LICENSE file.
 # ==============================================================================================
 
-set -u
+# Detect in some common error conditions.
+set -o nounset
+set -o pipefail
 
+# Cache result ito temp file since it is a slow operation
 declare tmpfilename=$(mktemp /tmp/mplist.XXXXXXXXXXXXX)
-multipass list > $tmpfilename
+multipass list >$tmpfilename
 
+# Exit handler
 function cleanup {
-  rm -f $tmpfilename
+    rm -f "$tmpfilename"
 }
 
 trap cleanup EXIT
@@ -34,11 +38,33 @@ infolog() {
 
 # Get version from the one true source - the makefile
 printversion() {
-  declare vers=$(grep DIST_VERSION Makefile | head -1 | awk '{printf "v" $3 }')
-  declare name=$(basename $0)
-  infolog "Name: ${name}\n"
-  infolog "Version: ${vers}\n"
+    declare vers
+    if vers=$(grep DIST_VERSION Makefile | head -1 | awk '{printf "v" $3 }'); then
+        errlog "Internal error. Failed to extract version from Makefile. Please report!"
+        exit 1
+    fi
+    declare name
+    name=$(basename "$0")
+    infolog "Name: ${name}\n"
+    infolog "Version: ${vers}\n"
 }
+
+# arg1 word to find
+# arg2 list to check from. Should be passed as "${list[@]}"
+exist_in_list() {
+    local -i found=0
+    local find="$1"
+    shift
+    local list=("$@")
+    for v in "${list[@]}"; do
+        if [[ $v == "$find" ]]; then
+            found=1
+            break
+        fi
+    done
+    return $found
+}
+
 
 usage() {
     declare name=$(basename $0)
@@ -56,57 +82,61 @@ EOT
 
 # Check we are executing from the mptools directory
 if [[ ! -d ../mptools || ! -f ../mptools/Makefile ]]; then
-  errlog "Must be executed from the mptools directory."
-  exit 1
+    errlog "Must be executed from the mptools directory."
+    exit 1
 fi
 
 declare -i quiet_flag=0
 declare nodes=""
+declare nodeList=("")
 
 while [[ $OPTIND -le "$#" ]]; do
     if getopts svh o; then
         case "$o" in
-        v)
-            printversion $0
-            exit 0
-            ;;
-        h)
-            usage $0
-            exit 0
-            ;;
-        s)
-            quiet_flag=1
-            ;;
-        [?])
-            usage "$(basename $0)"
-            exit 1
-            ;;
+            v)
+                printversion "$0"
+                exit 0
+                ;;
+            h)
+                usage "$0"
+                exit 0
+                ;;
+            s)
+                quiet_flag=1
+                ;;
+            [?])
+                usage "$(basename "$0")"
+                exit 1
+                ;;
         esac
     elif [[ $OPTIND -le "$#" ]]; then
         nodeName="${!OPTIND}"
-        if [[ ! "$nodeName" =~ ^ub(22|18|20)[bmf][smlexh][0-9]{2}$ ]] ; then
-            errlog "Node name not in recognised format ub<18|20|22><b|m|f|><s|m|l|x|h><NODENUMBER>: \"$i\""
+        if [[ ! "$nodeName" =~ ^ub(22|18|20)[bmf][smlexh][0-9]{2}$ ]]; then
+            errlog "Node name \"$nodeName\" not in recognised format ub<18|20|22><b|m|f|><s|m|l|x|h><NODENUMBER>"
             exit 1
         fi
 
         # Check if this node already exist
-        if cat $tmpfilename | grep $nodeName >/dev/null; then
-          errlog "Node $nodeName already exists, skipping."
+        if grep $nodeName < "$tmpfilename" >/dev/null; then
+            errlog "Node $nodeName already exists, skipping."
         else
-          nodes+="$nodeName "
+            if ! exist_in_list ${nodeName} "${nodeList[@]}"; then
+                errlog "Same node name specified more than once \"${nodeName}\""
+                exit 1
+            fi
+            nodeList+=("$nodeName")
+            nodes+="$nodeName "
         fi
         ((OPTIND++))
     fi
 done
 if [[ -z $nodes ]]; then
-  infolog "No nodes to create.\n"
-  exit 1
+    infolog "No nodes to create.\n"
+    exit 1
 fi
 
 if [[ $quiet_flag -eq 1 ]]; then
-  make -s NODES="$nodes" node
+   echo make -s NODES="${nodes}" node
 else
-  make NODES="${nodes}" node
+   echo make NODES="${nodes}" node
 fi
-
-
